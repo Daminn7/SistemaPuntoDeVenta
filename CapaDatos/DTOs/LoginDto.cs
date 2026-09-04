f4ff81f7a23cc32b27c02a32a7ce0bac9b32d986
pablo <mansillapablo1987@gmai.com>
2026-09-04 17:43:01 -0300

    creacion de capa logica y  datos

---
+++ CapaDatos/DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CapaDatos.DTOs
+{
+    public class LoginDto
+    {
+        public string CodUsuario { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
