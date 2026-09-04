0b132c07b8b6eaffbd2d8f0940cf602d0a4e6240
Flor Cocomarola <ingeelect92@gmail.com>
2026-09-04 17:53:14 -0300

    Descomentado arranque

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -16,7 +16,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());//;FormPrincipal("Gaston", "Administrador")
+            Application.Run(new FormLogin());
         }
     }
 }
